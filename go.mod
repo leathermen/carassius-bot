33a37c57e46ee7723e75b0db03d59ca5d1d02c7a
nikitades <nikitades@pm.me>
2024-10-05 22:52:03 +0200

    TIC

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/nikitades/carassius-bot
+
+go 1.23.0
+
+require github.com/thanhpk/randstr v1.0.6 // indirect
---
+++ producer/cmd/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/thanhpk/randstr"
+)
+
+func main() {
+	fmt.Println(randstr.String(10))
+}
