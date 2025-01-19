9dc99b7890d1ed7de21ee7c0b827ca1ec4766da0
nikitades <nikitades@pm.me>
2025-01-19 12:27:11 +0100

    Extend the video size limit

--- consumer/internal/handlers/youtube/yt.go
+++ consumer/internal/handlers/youtube/yt.go
@@ -26,27 +26,42 @@ const (
 	Code = "youtube"
 
 	maxVideoFileSize  = 1024 * 1024 * 2
-	maxAudioTrackSize = 1024 * 1024 * 0.5
+	maxAudioTrackSize = 1024 * 1024 * 1.5
+
+	maxJointFileSize = 1024 * 1024 * 5
 )
 
 type Handler struct {
-	bot *telego.Bot
-	q   queue.Queue
-	db  db.Database
-	pp  *handler.Proxy
+	bot     *telego.Bot
+	q       queue.Queue
+	db      db.Database
+	pp      *handler.Proxy
+	botname *telego.BotName
 
 	channels []int64
 }
 
 func New(bot *telego.Bot, q queue.Queue, db db.Database, proxyParams *handler.Proxy, channels []int64) *Handler {
-	return &Handler{bot, q, db, proxyParams, channels}
+	handler := &Handler{
+		bot:      bot,
+		q:        q,
+		db:       db,
+		pp:       proxyParams,
+		channels: channels,
+		botname:  nil,
+	}
+
+	botname, _ := handler.bot.GetMyName(nil)
+	handler.botname = botname
+
+	return handler
 }
 
 func (h *Handler) Name() string {
 	return Code
 }
 
-func (h *Handler) Handle(userID int64, msg string, msgID int) error { //nolint:gocyclo
+func (h *Handler) Handle(userID int64, msg string, msgID int) error {
 	defer func() {
 		if err := h.q.DeleteMessageFromQueue(msgID); err != nil {
 			log.Printf("failed to remove message from queue: %d", msgID)
@@ -60,8 +75,7 @@ func (h *Handler) Handle(userID int64, msg string, msgID int) error { //nolint:g
 		return handler.ErrFailedToGetMedia
 	}
 
-	botname, _ := h.bot.GetMyName(nil)
-	mediaFile, err := h.db.GetMediaFileBySocialNetworkID(videoID, request.TypeYoutube.String(), botname.Name)
+	mediaFile, err := h.db.GetMediaFileBySocialNetworkID(videoID, request.TypeYoutube.String(), h.botname.Name)
 
 	if err != nil {
 		log.Printf("failed to lookup media files DB: %s", err)
@@ -121,6 +135,23 @@ func (h *Handler) Handle(userID int64, msg string, msgID int) error { //nolint:g
 		return handler.ErrFailedToGetMedia
 	}
 
+	separateVideo, separateAudio, found := h.findSeparateFormats(video)
+
+	if found {
+		return h.replyMerged(video, &separateVideo, &separateAudio, userID, &client)
+	}
+
+	singleVideo, found := h.findSingleFormat(video)
+
+	if found {
+		return h.replySingle(video, &singleVideo, userID, &client)
+	}
+
+	log.Printf("failed to find appropriate youtube video formats")
+	return handler.ErrFailedToGetMedia
+}
+
+func (h *Handler) findSeparateFormats(video *youtube.Video) (videoFormat, audioFormat youtube.Format, found bool) {
 	var optimalVideoFormat youtube.Format
 	var optimalAudioFormat youtube.Format
 
@@ -143,15 +174,45 @@ func (h *Handler) Handle(userID int64, msg string, msgID int) error { //nolint:g
 	}
 
 	if optimalVideoFormat.ContentLength == 0 {
-		log.Printf("no appropriate video track found: %s", msg)
-		return handler.ErrFailedToGetMedia
+		log.Printf("no appropriate separate video track found")
+		return youtube.Format{}, youtube.Format{}, false
 	}
 
 	if optimalAudioFormat.ContentLength == 0 {
-		log.Printf("no appropriate audio track found: %s", msg)
-		return handler.ErrFailedToGetMedia
+		log.Printf("no appropriate separate audio track found")
+		return optimalVideoFormat, youtube.Format{}, false
 	}
 
+	return optimalVideoFormat, optimalAudioFormat, true
+}
+
+func (h *Handler) findSingleFormat(video *youtube.Video) (youtube.Format, bool) {
+	var optimalVideoFormat youtube.Format
+
+	withAudio := []youtube.Format{}
+
+	for _, v := range video.Formats {
+		isMP4 := strings.Contains(v.MimeType, "video/mp4")
+		isLessThanLimit := v.ContentLength <= maxJointFileSize
+		isMoreThanTheLastOne := v.ContentLength > optimalVideoFormat.ContentLength
+		hasAudioTrack := v.AudioQuality != ""
+		if hasAudioTrack {
+			withAudio = append(withAudio, v)
+		}
+		if isMP4 && isLessThanLimit && isMoreThanTheLastOne && hasAudioTrack {
+			optimalVideoFormat = v
+		}
+	}
+
+	if optimalVideoFormat.ContentLength == 0 {
+		log.Printf("no appropriate joint video track found")
+		return youtube.Format{}, false
+	}
+
+	return optimalVideoFormat, true
+}
+
+func (h *Handler) replyMerged(video *youtube.Video, videoFormat, audioFormat *youtube.Format, userID int64, client *youtube.Client) error {
 	file, err := os.CreateTemp("", randstr.String(32)+".mp4")
 	if err != nil {
 		log.Printf("failed to create a tmp video file: %v", err)
@@ -160,7 +221,7 @@ func (h *Handler) Handle(userID int64, msg string, msgID int) error { //nolint:g
 	defer file.Close()
 
 	// Download the video content
-	stream, _, err := client.GetStream(video, &optimalVideoFormat)
+	stream, _, err := client.GetStream(video, videoFormat)
 	if err != nil {
 		log.Printf("failed to download video: %v", err)
 		return handler.ErrFailedToGetMedia
@@ -182,7 +243,7 @@ func (h *Handler) Handle(userID int64, msg string, msgID int) error { //nolint:g
 	defer audiofile.Close()
 
 	// Download the audio content
-	stream, _, err = client.GetStream(video, &optimalAudioFormat)
+	stream, _, err = client.GetStream(video, audioFormat)
 	if err != nil {
 		log.Printf("failed to download audio: %v", err)
 		return handler.ErrFailedToGetMedia
@@ -217,8 +278,8 @@ func (h *Handler) Handle(userID int64, msg string, msgID int) error { //nolint:g
 	params := telego.SendVideoParams{
 		ChatID: telegoutil.ID(userID),
 		Video:  inputfile,
-		Width:  optimalVideoFormat.Width,
-		Height: optimalVideoFormat.Height,
+		Width:  videoFormat.Width,
+		Height: videoFormat.Height,
 	}
 
 	tgMsg, err := h.bot.SendVideo(&params)
@@ -231,19 +292,83 @@ func (h *Handler) Handle(userID int64, msg string, msgID int) error { //nolint:g
 		if _, err = h.bot.SendVideo(&telego.SendVideoParams{
 			ChatID: telegoutil.ID(c),
 			Video:  telegoutil.FileFromID(tgMsg.Video.FileID),
-			Width:  optimalVideoFormat.Width,
-			Height: optimalVideoFormat.Height,
+			Width:  videoFormat.Width,
+			Height: videoFormat.Height,
 		}); err != nil {
 			log.Printf("failed to send tg video to channel %d: %s", c, err)
 		}
 	}
 
 	mediaFileData := queue.MediaFile{
-		SocialNetworkID:   videoID,
+		SocialNetworkID:   video.ID,
 		SocialNetworkName: Code,
 		FileID:            tgMsg.Video.FileID,
 		FileType:          "video",
-		Bot:               botname.Name,
+		Bot:               h.botname.Name,
+	}
+
+	if err := h.db.InsertMediaFile(mediaFileData); err != nil {
+		log.Printf("failed to save yt media post download: %s", err)
+	}
+
+	return nil
+}
+
+func (h Handler) replySingle(video *youtube.Video, videoFormat *youtube.Format, userID int64, client *youtube.Client) error {
+	file, err := os.CreateTemp("", randstr.String(32)+".mp4")
+	if err != nil {
+		log.Printf("failed to create a tmp video file: %v", err)
+		return handler.ErrFailedToGetMedia
+	}
+	defer file.Close()
+
+	// Download the video content
+	stream, _, err := client.GetStream(video, videoFormat)
+	if err != nil {
+		log.Printf("failed to download video: %v", err)
+		return handler.ErrFailedToGetMedia
+	}
+
+	_, err = io.Copy(file, stream)
+	if err != nil {
+		log.Printf("failed to copy video file: %v", err)
+		return handler.ErrFailedToGetMedia
+	}
+
+	defer file.Close()
+
+	inputfile := telego.InputFile{File: file}
+
+	params := telego.SendVideoParams{
+		ChatID: telegoutil.ID(userID),
+		Video:  inputfile,
+		Width:  videoFormat.Width,
+		Height: videoFormat.Height,
+	}
+
+	tgMsg, err := h.bot.SendVideo(&params)
+	if err != nil {
+		log.Printf("failed to send tg video to user: %s", err)
+		return handler.ErrFailedToGetMedia
+	}
+
+	for _, c := range h.channels {
+		if _, err = h.bot.SendVideo(&telego.SendVideoParams{
+			ChatID: telegoutil.ID(c),
+			Video:  telegoutil.FileFromID(tgMsg.Video.FileID),
+			Width:  videoFormat.Width,
+			Height: videoFormat.Height,
+		}); err != nil {
+			log.Printf("failed to send tg video to channel %d: %s", c, err)
+		}
+	}
+
+	mediaFileData := queue.MediaFile{
+		SocialNetworkID:   video.ID,
+		SocialNetworkName: Code,
+		FileID:            tgMsg.Video.FileID,
+		FileType:          "video",
+		Bot:               h.botname.Name,
 	}
 
 	if err := h.db.InsertMediaFile(mediaFileData); err != nil {
